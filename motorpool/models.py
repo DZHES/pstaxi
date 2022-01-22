caee59731dffc86effb1b01d8d3c0e7b8eb4b06d
Esentur <esentur1997@gmail.com>
2022-01-21 23:12:34 -0900

    Second commit

--- motorpool/models.py
+++ motorpool/models.py
@@ -10,3 +10,25 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.title
+
+class Auto(models.Model):
+    AUTO_CLASS_ECONOMY = 'e'
+    AUTO_CLASS_COMFORT = 'c'
+    AUTO_CLASS_BUSINESS = 'b'
+
+    AUTO_CLASS_CHOICES = (
+        (AUTO_CLASS_ECONOMY, 'economy'),
+        (AUTO_CLASS_COMFORT, 'comfort'),
+        (AUTO_CLASS_BUSINESS, 'business'),
+    )
+    number = models.CharField(max_length=15)
+    description = models.TextField(max_length=500, default='', blank=True)
+    year = models.PositiveSmallIntegerField(null=True)
+    auto_class = models.CharField(max_length=1, null=True, choices=AUTO_CLASS_CHOICES, default=AUTO_CLASS_ECONOMY)
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name_plural = 'Автомобили'
+        verbose_name = 'Автомобиль'
